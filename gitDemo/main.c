e533afa687d1884a30b10c7aa9f6abb61b79f86d
Ryan Downey <ryandowney@Ryans-MacBook-Pro.local>
2017-01-03 14:09:14 -0700

    Initial Commit

---
+++ gitDemo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gitDemo
+//
+//  Created by Ryan Downey on 1/3/17.
+//  Copyright © 2017 Ryan Downey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
